2ce306dfc6437184744ba0c4165721373e19a956
¨afandino21¨ <al_fl95@hotmail.com>
2023-11-01 14:02:02 -0300

    Se cumple con casi toda la consigna
    Hay que hacer que no abra el modal en algunos casos
    
    Co-Authored-By: Franco <113482175+francordz99@users.noreply.github.com>
    Co-Authored-By: Marcos Araujo <140938264+mrk4os@users.noreply.github.com>
    Co-Authored-By: João Lizot <141048911+joaolizot@users.noreply.github.com>

--- 18.2.1-backend/public/scripts/index.js
+++ 18.2.1-backend/public/scripts/index.js
@@ -5,54 +5,69 @@ const btnPost = document.getElementById('btnPost');
 const btnPut = document.getElementById('btnPut');
 const btnDelete = document.getElementById('btnDelete');
 const btnSendChanges = document.getElementById('btnSendChanges')
+//INPUTS
+let userId = document.getElementById("inputGet1Id");
+let inputPostNombre = document.getElementById("inputPostNombre");
+let inputPostApellido = document.getElementById("inputPostApellido");
+let inputPutId = document.getElementById("inputPutId");
+let inputDelete = document.getElementById("inputDelete");
+
+function validateInput() {
+    if (inputPostNombre.value === "" || inputPostApellido.value === "") {
+        btnPost.disabled = true;
+    } else {
+        btnPost.disabled = false;
+    }
+}
+
+inputPostNombre.addEventListener('input', validateInput);
+inputPostApellido.addEventListener('input', validateInput);
+
+
+// document.getElementById('inputPostNombre').addEventListener('input', () => {
+//     if (inputPostNombre.value === "" || inputPostApellido.value === "") {
+//         btnPost.disabled = true;
+//     } else {
+//         btnPost.disabled = false;
+//     }
+// });
+
+// document.getElementById('inputPostApellido').addEventListener('input', () => {
+
+//     if (inputPostNombre.value === "" || inputPostApellido.value === "") {
+//         btnPost.disabled = true;
+//     } else {
+//         btnPost.disabled = false;
+//     }
+// })
+
+document.getElementById('inputPutId').addEventListener('input', () => {
+    if (inputPutId.value === "") {
+        btnPut.disabled = true;
+        console.log('cambio a inhabilitado');
+    } else {
+        btnPut.disabled = false;
+
+        console.log('cambio a habilitado');
+    }
+})
+
+document.getElementById('inputDelete').addEventListener('input', () => {
+    if (inputDelete === "") {
+
+        btnDelete.disabled = true;
+    } else {
+        btnDelete.disabled = false;
+    }
+})
+
 
 // Buscar Registro
 const results = document.getElementById("results");
 botonGet1.addEventListener('click', () => {
     let userId = document.getElementById("inputGet1Id").value;
     console.log(userId);
-    if (userId === "") {
-        console.log(url + "users");
-        fetch(url + "users")
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-
-                let texto = "";
-                data.forEach(item => {
-                    texto += `ID: ${item.id}
-                NAME: ${item.name}
-                LASTNAME: ${item.lastname}
-                `;
-                });
-                results.innerText = texto;
-
-            })
-            .catch(error => {
-                console.log('No se pudieron cargar los datos');
-            });
-    } else {
-        fetch(`https://6542337bf0b8287df1ffada7.mockapi.io/users/${userId}`)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                let texto = "";
-                texto = `
-            ID: ${data.id}
-            NAME: ${data.name}
-            LASTNAME: ${data.lastname}
-
-            `;
-
-                results.innerText = texto;
-
-            })
-            .catch(error => {
-                console.log('No se pudo cargar la id');
-            });
-    }
+    showList(userId);
 });
 
 // Ingresar nuevo registro
@@ -98,39 +113,44 @@ btnPost.addEventListener('click', () => {
         `;
             results.innerText = texto;
 
-            fetch(urlCompleta)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-
-                    let texto = "";
-                    data.forEach(item => {
-                        texto += `
-                ID: ${item.id}
-                NAME: ${item.name}
-                LASTNAME: ${item.lastname}
-                `;
-                    });
-                    results.innerText = texto;
-
-                })
-                .catch(error => {
-                    console.log('No se puede mostrar la lsita de usuarios');
-                });
-
+            showList("");
         })
         .catch(error => {
-            // Maneja errores de red u otros errores
             console.error('Error:', error);
+
+            alert("Error al agregar nuevo usuario.");
         });
 });
 
-// MODIFICAR
+// Modificar Registro
+
+// Listener para boton Modificar
 btnPut.addEventListener('click', () => {
-    const inputPutId = document.getElementById("inputPutId").value;
-    const inputPutNombre = document.getElementById("inputPutNombre").value;
-    const inputPutApellido = document.getElementById("inputPutApellido").value;
+    inputPutId = document.getElementById("inputPutId").value;
+    let inputPutNombre = document.getElementById("inputPutNombre").value;
+    let inputPutApellido = document.getElementById("inputPutApellido").value;
+    fetch(`https://6542337bf0b8287df1ffada7.mockapi.io/users/${inputPutId}`)
+        .then(response => {
+            if (!response.ok) {
+                // Si la respuesta del servidor no es exitosa, lanza un error
+                throw new Error(`No se pudo cargar la ID: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById("inputPutNombre").value = data.name;
+            document.getElementById("inputPutApellido").value = data.lastname;
+        })
+        .catch(error => {
+            // Muestra una alerta al usuario
+            alert("Usuario no encontrado.");
+        });
+});
+// Listener para boton Guardar
+btnSendChanges.addEventListener('click', () => {
+    let inputPutId = document.getElementById("inputPutId").value;
+    let inputPutNombre = document.getElementById("inputPutNombre").value;
+    let inputPutApellido = document.getElementById("inputPutApellido").value;
 
     if (inputPutId) {
         const ruta = `/users/${inputPutId}`; // Ruta con el ID del usuario a modificar
@@ -146,7 +166,6 @@ btnPut.addEventListener('click', () => {
             },
             body: JSON.stringify(updatedUser), // Convierte el objeto a formato JSON
         };
-
         const urlCompleta = url + ruta;
 
         fetch(urlCompleta, opciones)
@@ -154,14 +173,15 @@ btnPut.addEventListener('click', () => {
                 if (!response.ok) {
                     throw new Error('Error al modificar el usuario');
                 }
-                return response.json();
+                response.json();
+                showList("");
             })
             .then(data => {
                 // Mostrar el resultado de la modificación
                 console.log(data);
             })
             .catch(error => {
-                console.error('Error:', error);
+                alert("Usuario no encontrado.");
             });
     } else {
         alert("Por favor, ingrese un ID válido para la modificación.");
@@ -182,18 +202,78 @@ btnDelete.addEventListener('click', () => {
                 if (response.ok) {
                     // Lógica para manejar el éxito (por ejemplo, actualizar la lista en el HTML)
                     // También puedes limpiar el campo de entrada
-                    document.getElementById("inputDelete").value = '';
+                    showList("");
                 } else {
                     // Manejar el error y mostrar una alerta
-                    alert("Error al eliminar el usuario.");
+                    alert("Usuario no encontrado");
                 }
             })
             .catch(error => {
                 // Manejar el error y mostrar una alerta
-                alert("Error de red al intentar eliminar el usuario.");
+                alert("Error al eliminar el usuario");
             });
-    } else {
-        // Mostrar un mensaje al usuario indicando que debe ingresar un ID
-        alert("Por favor, ingrese un ID válido para la eliminación.");
     }
 });
+
+
+function showList(userId) {
+    if (userId === "") {
+        fetch(url + "users")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+
+                let texto = "";
+                data.forEach(item => {
+                    texto += `ID: ${item.id}
+                NAME: ${item.name}
+                LASTNAME: ${item.lastname}
+                `;
+                });
+                results.innerText = texto;
+
+            })
+            .catch(error => {
+                console.log('No se pudieron cargar los datos');
+            });
+    } else {
+        fetch(`https://6542337bf0b8287df1ffada7.mockapi.io/users/${userId}`)
+            .then(response => {
+                if (!response.ok) {
+                    // Si la respuesta del servidor no es exitosa, lanza un error
+                    throw new Error(`No se pudo cargar la ID: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                let texto = `
+                ID: ${data.id}
+                NAME: ${data.name}
+                LASTNAME: ${data.lastname}
+            `;
+                results.innerText = texto;
+            })
+            .catch(error => {
+                // Muestra una alerta al usuario
+                alert("Usuario no encontrado.");
+            });
+    }
+};
+
+// function validateID(userId) {
+//     let largoRegistro = 0;
+//     fetch(url + "users")
+//         .then(response => {
+//             return response.json();
+//         })
+//         .then(data => {
+//             largoRegistro = data.length;
+//             if (userId > largoRegistro) {
+//                 alert("Error, usuario no existente.");
+//             }
+//         })
+// }
+// validateID(50);
+
+
